e7f13289dfa13892799cb18e90627952c94bba67
TOVTC <veronica.tc.to@outlook.com>
2022-10-18 19:40:46 -0400

    check if two Strings are anagrams

---
+++ src/CheckAnagram.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class CheckAnagram {
+    public static void main(String[] args) {
+        System.out.println(anagramCheck("star", "RaTs"));
+        System.out.println(anagramCheck("seldOm", "models"));
+        System.out.println(anagramCheck("parallel", "height"));
+    }
+
+    public static boolean anagramCheck(String first, String second) {
+        String fw = first.toLowerCase();
+        String sw = second.toLowerCase();
+
+        if (fw.length() != sw.length()) {
+            return false;
+        }
+
+        char[] fwArr = fw.toCharArray();
+        char[] swArr = sw.toCharArray();
+
+        Arrays.sort(fwArr);
+        Arrays.sort(swArr);
+
+        for (int i = 0; i < fwArr.length; i++) {
+            if (fwArr[i] != swArr[i]) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
